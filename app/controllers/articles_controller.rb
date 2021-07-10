1ae7ea3770113cd1a8a4d20b6a813908d302ad32
TerloevIslam <terloevislam89@gmail.com>
2021-07-10 21:48:47 +0200

    Adjust path helpers on views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,62 +1,60 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[show edit update destroy]
+  before_action :get_user, excepct: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /articles or /articles.json
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1 or /articles/1.json
   def show
   end
 
-  # GET /articles/new
   def new
-    @article = current_user.articles.build
+    @article = @user.articles.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles or /articles.json
   def create
-    @article = current_user.articles.new(article_params)
-
+    @article = @user.articles.new(article_params)
     respond_to do |format|
       if @article.save
         format.html { redirect_to root_path, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
+        format.html { redirect_to root_path, notice: "Article was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
     respond_to do |format|
       format.html { redirect_to root_path, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
+
+    def get_user
+      @user =
+        if params[:action] == 'show'
+          User.find(params[:user_id])
+        else
+          current_user
+        end
+    end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_article
